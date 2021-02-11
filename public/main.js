c2fd05c96d7f7171269d16cdce793e5ee6105c7c
Rik de Wit <rikdewit1997@gmail.com>
2021-02-11 15:04:34 +0100

    added chess.js logic

--- public/main.js
+++ public/main.js
@@ -1,39 +1,88 @@
-function onDragStart (source, piece, position, orientation) {
-    console.log('Drag started:')
-    console.log('Source: ' + source)
-    console.log('Piece: ' + piece)
-    console.log('Position: ' + Chessboard.objToFen(position))
-    console.log('Orientation: ' + orientation)
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-  }
 
-  function onChange (oldPos, newPos) {
-    console.log('Position changed:')
-    console.log('Old position: ' + Chessboard.objToFen(oldPos))
-    console.log('New position: ' + Chessboard.objToFen(newPos))
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-  }
 
 var config = {
     pieceTheme: 'img/chesspieces/wikipedia/{piece}.png',
     position: 'start',
-    draggable: 'true',
+    draggable: true,
     onDragStart: onDragStart,
-    onChange: onChange,
-    sparePieces: false
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd,
   }
-var board = Chessboard('myBoard', config)
 
-$('#setRuyLopezBtn').on('click', function () {
-    board.position('r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R')
-  })
   
-  $('#setStartBtn').on('click', board.start)
+
+
+  var board = null
+  var game = new Chess()
+  var $status = $('#status')
+  var $fen = $('#fen')
+  var $pgn = $('#pgn')
   
-  $('#setRookCheckmateBtn').on('click', function () {
-    board.position({
-      a4: 'bK',
-      c4: 'wK',
-      a7: 'wR'
+  function onDragStart (source, piece, position, orientation) {
+    // do not pick up pieces if the game is over
+    if (game.game_over()) return false
+  
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      return false
+    }
+  }
+  
+  function onDrop (source, target) {
+    // see if the move is legal
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
     })
-  })
+  
+    // illegal move
+    if (move === null) return 'snapback'
+  
+    updateStatus()
+  }
+  
+  // update the board position after the piece snap
+  // for castling, en passant, pawn promotion
+  function onSnapEnd () {
+    board.position(game.fen())
+  }
+  
+  function updateStatus () {
+    var status = ''
+  
+    var moveColor = 'White'
+    if (game.turn() === 'b') {
+      moveColor = 'Black'
+    }
+  
+    // checkmate?
+    if (game.in_checkmate()) {
+      status = 'Game over, ' + moveColor + ' is in checkmate.'
+    }
+  
+    // draw?
+    else if (game.in_draw()) {
+      status = 'Game over, drawn position'
+    }
+  
+    // game still on
+    else {
+      status = moveColor + ' to move'
+  
+      // check?
+      if (game.in_check()) {
+        status += ', ' + moveColor + ' is in check'
+      }
+    }
+  
+    $status.html(status)
+    $fen.html(game.fen())
+    $pgn.html(game.pgn())
+  }
+  
+
+  board = Chessboard('myBoard', config)
+  
+  updateStatus()
