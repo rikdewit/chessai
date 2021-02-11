63250345b329955cc1b49bd3700a458702150c91
Rik de Wit <rikdewit1997@gmail.com>
2021-02-11 12:51:38 +0100

    chessboard ui

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public/'))
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile("index.html")
 })
 
 app.listen(port, () => {
---
+++ public/main.js
@@ -0,0 +1,7 @@
+
+var config = {
+    pieceTheme: 'img/chesspieces/wikipedia/{piece}.png',
+    position: 'start'
+  }
+var board = Chessboard('myBoard', config)
+
